123bcfe1041d90e32431c61c828c7b9c7c3a5741
priyankayadav90 <priyankay035024@gmail.com>
2026-09-20 15:54:08 +0530

    Time: 0 ms (100%), Space: 20.7 MB (17.04%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,10 @@
+import bisect
+from typing import List
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        
+        idx = bisect.bisect_right(letters, target)
+        
+        
+        return letters[idx % len(letters)]
